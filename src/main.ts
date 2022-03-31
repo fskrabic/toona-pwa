5392436034327774b2f046b23bad809957ea0ba9
Filip Skrabic <filip.skrabic@avl.com>
2022-03-31 12:07:08 +0200

    removed unnecessary code from main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { ApplicationRef, enableProdMode } from '@angular/core';
-import { enableDebugTools } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -9,17 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule).then((moduleRef) => {
-  const applicationRef = moduleRef.injector.get(ApplicationRef);
-  const originalTick = applicationRef.tick;
-    applicationRef.tick = function() {
-      const windowPerfomance = window.performance;
-      const  before = windowPerfomance.now();
-      const retValue = originalTick.apply(this, arguments);
-      const after = windowPerfomance.now();
-      const runTime = after - before;
-      window.console.log('CHANGE DETECTION TIME' , runTime);
-      return retValue;
-    };
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
   navigator.serviceWorker.register('/ngsw-worker.js');
 }).catch(err => console.log(err));
