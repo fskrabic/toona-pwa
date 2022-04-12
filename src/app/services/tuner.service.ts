9afb80c741660a9cd2befc356658f70d596cee4b
fskrabic <skrabicf@gmail.com>
2022-04-12 10:50:06 +0200

    reverting changes

--- src/app/services/tuner.service.ts
+++ src/app/services/tuner.service.ts
@@ -38,7 +38,7 @@ export class TunerService implements OnDestroy {
     if (this.audioContext.state !== 'running') {
       this.audioContext.resume();
     }
-    const src = interval(200);
+    const src = interval(350);
     if (!this.pitchSubscription) {
       this.pitchSubscription = src.subscribe(this.getPitch);
     }
--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  console.log = () => {
-    return null;
-  };
-  console.warn = () => {
-    return null;
-  };
-  window.console.log = () => {
-    return null;
-  };
-  window.console.warn = () => {
-    return null;
-  };
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .then(() => {
-    navigator.serviceWorker.register('/ngsw-worker.js');
-  })
-  .catch((err) => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+  navigator.serviceWorker.register('/ngsw-worker.js');
+}).catch(err => console.log(err));
