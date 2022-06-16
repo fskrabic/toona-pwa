fd051b09029fc5a6a129265bdb5268cbc764ce57
fskrabic <skrabicf@gmail.com>
2022-06-16 19:53:15 +0200

    added note constants

---
+++ src/app/services/note.consts.ts
@@ -0,0 +1,199 @@
+import { Note } from '../components/tuner/tuner.component';
+
+export const ChromaticNotes: Note[] = [
+  {
+    note: 'C1',
+    freq: 32.7,
+  },
+  {
+    note: 'C#1',
+    freq: 34.65,
+  },
+  {
+    note: 'D1',
+    freq: 36.71,
+  },
+  {
+    note: 'D#1',
+    freq: 38.89,
+  },
+  {
+    note: 'E1',
+    freq: 41.2,
+  },
+  {
+    note: 'F1',
+    freq: 43.65,
+  },
+  {
+    note: 'F#1',
+    freq: 46.25,
+  },
+  {
+    note: 'G1',
+    freq: 49,
+  },
+  {
+    note: 'G#1',
+    freq: 51.91,
+  },
+  {
+    note: 'A1',
+    freq: 55,
+  },
+  {
+    note: 'A#1',
+    freq: 58.27,
+  },
+  {
+    note: 'B1',
+    freq: 61.74,
+  },
+
+  {
+    note: 'C2',
+    freq: 65.41,
+  },
+  {
+    note: 'C#2',
+    freq: 69.3,
+  },
+  {
+    note: 'D2',
+    freq: 73.42,
+  },
+  {
+    note: 'D#2',
+    freq: 77.78,
+  },
+  {
+    note: 'E2',
+    freq: 82.41,
+  },
+  {
+    note: 'F2',
+    freq: 87.31,
+  },
+  {
+    note: 'F#2',
+    freq: 92.5,
+  },
+  {
+    note: 'G2',
+    freq: 98,
+  },
+  {
+    note: 'G#2',
+    freq: 103.83,
+  },
+  {
+    note: 'A2',
+    freq: 110,
+  },
+  {
+    note: 'A#2',
+    freq: 116.54,
+  },
+  {
+    note: 'B2',
+    freq: 123.47,
+  },
+
+  {
+    note: 'C3',
+    freq: 130.81,
+  },
+  {
+    note: 'C#3',
+    freq: 138.59,
+  },
+  {
+    note: 'D3',
+    freq: 146.83,
+  },
+  {
+    note: 'D#3',
+    freq: 155.56,
+  },
+  {
+    note: 'E3',
+    freq: 164.81,
+  },
+  {
+    note: 'F3',
+    freq: 174.61,
+  },
+  {
+    note: 'F#3',
+    freq: 185,
+  },
+  {
+    note: 'G3',
+    freq: 196,
+  },
+  {
+    note: 'G#3',
+    freq: 207.65,
+  },
+  {
+    note: 'A3',
+    freq: 220,
+  },
+  {
+    note: 'A#3',
+    freq: 233.08,
+  },
+  {
+    note: 'B3',
+    freq: 246.94,
+  },
+
+  {
+    note: 'C4',
+    freq: 261.63,
+  },
+  {
+    note: 'C#4',
+    freq: 277.18,
+  },
+  {
+    note: 'D4',
+    freq: 293.66,
+  },
+  {
+    note: 'D#4',
+    freq: 311.13,
+  },
+  {
+    note: 'E4',
+    freq: 329.63,
+  },
+  {
+    note: 'F4',
+    freq: 349.23,
+  },
+  {
+    note: 'F#4',
+    freq: 369.99,
+  },
+  {
+    note: 'G4',
+    freq: 392,
+  },
+  {
+    note: 'G#4',
+    freq: 415.3,
+  },
+  {
+    note: 'A4',
+    freq: 440,
+  },
+  {
+    note: 'A#4',
+    freq: 466.16,
+  },
+  {
+    note: 'B4',
+    freq: 493.88,
+  },
+];
